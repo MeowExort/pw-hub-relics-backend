cc9b1b5f3c63b9db61b4896e7cab77a99039fb65
MeowExort <tobilts@yandex.ru>
2026-02-20 15:08:05 +0300

    refactor(api): удалить telegram_chat_id из notification_filters

---
+++ src/Pw.Hub.Relics.Infrastructure/Data/Migrations/20260220120616_RemoveTelegramChatIdFromNotificationFilters.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pw.Hub.Relics.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTelegramChatIdFromNotificationFilters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "telegram_chat_id",
+                table: "notification_filters");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "telegram_chat_id",
+                table: "notification_filters",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
